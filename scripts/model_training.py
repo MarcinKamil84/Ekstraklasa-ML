3d54a3171f9a469b219fb46ae1df7a68ecc77151
Marcin Świostek <99032084+MarcinKamil84@users.noreply.github.com>
2023-04-08 01:39:36 +0200

    Update model_training.py

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -1,87 +1,131 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from yellowbrick.classifier import confusion_matrix
+from yellowbrick.classifier.rocauc import roc_auc
+from pycaret.classification import *
+
+
 # IMPORTING DATA
 
-X_G_bal = pd.read_pickle('./data/X_G_bal.pkl')
-X_Gl_bal = pd.read_pickle('./data/X_Gl_bal.pkl')
+ekstraklasa_balanced_train = pd.read_pickle('./data/inputs/ekstraklasa_train.pkl')
+
+ekstraklasa_balanced_train = ekstraklasa_balanced_train.loc[:,~ekstraklasa_balanced_train.T.duplicated(keep='first')]
 
 
-# FEATURES TYPES
+# PREPARING DATA
 
-cat_feats = ['home_or_away', 'season_ranks']
+X_pycaret_points = ekstraklasa_balanced_train.drop(columns=['goals', 'goals_lost', 'team', 'opponent'])
+X_pycaret_goals = ekstraklasa_balanced_train.drop(columns=['points', 'goals_lost', 'team', 'opponent'])
 
-num_feats = ['team', 'home_or_away', 'opponent', 'czas_delta', 'season_points'] + [col for col in X_G_bal.columns if '_g_' in col] + [col for col in X_G_bal.columns if '_gl_' in col]
+# PYCARET settings
+
+categorical_features = ['home_or_away']
+feature_selection_threshold = float((np.sqrt(len(X_pycaret_goals.columns))) / len(X_pycaret_goals.columns))
+
+
+# # POINTS
+
+# MODEL SELECTION
+print("Comparing models...")
+
+points = setup(
+    data = X_pycaret_points,
+    target = 'points',
+    silent = True,
+    polynomial_features = True,
+    categorical_features = categorical_features,
+    feature_selection = True,
+    feature_selection_threshold = feature_selection_threshold
+    )
+
+best_points = compare_models(n_select = 1, sort = 'AUC')
+
+results = pull(best_points)
+
+# MODEL TRAINING
+print("Model training...")
+
+points_holdout = predict_model(best_points)
+
+points_predictions = pull(best_points)
+
+points_created = create_model(best_points)
+
+roc_auc(points_created, get_config('X_train'), get_config('y_train'), X_test=get_config('X_test'), y_test=get_config('y_test'), classes=['2', 'x', '1'], show=False)
+plt.savefig('./models/points_auc.png')
+plt.clf()
+
+confusion_matrix(points_created, get_config('X_train'), get_config('y_train'), X_test=get_config('X_test'), y_test=get_config('y_test'), show=False)
+plt.savefig('./models/points_conf.png')
+plt.clf()
+
+
+# MODEL TUNING
+print("Model tuning...")
+
+points_tuned = tune_model(points_created, optimize='AUC', choose_better=True, n_iter = 50)
+
+points_final = finalize_model(points_tuned)
+
+points_all = predict_model(points_final)
+
+
+# SAVING MODEL
+print("Saving model...")
+save_model(points_final, './models/points_final')
+
+
+# # GOALS
+
+# MODEL SELECTION
+print("Comparing models...")
+
+
+goals = setup(
+    data = X_pycaret_goals,
+    target = 'goals',
+    silent = True,
+    polynomial_features = True,
+    categorical_features = categorical_features,
+    feature_selection = True,
+    feature_selection_threshold = feature_selection_threshold
+    )
+
+best_goals = compare_models(n_select = 1, sort = 'AUC')
 
 
 # MODEL TRAINING
+print("Model training...")
 
-# Points (win/draw/lose)
+goals_holdout = predict_model(best_goals)
 
-s_points = setup(data = X_G_bal, target = 'points',
-                 ignore_features=['goals', 'goals_lost'],
-                 silent=True,
-                 categorical_features = cat_feats,
-                 remove_multicollinearity = True)
+goals_predictions = pull(best_goals)
 
+goals_created = create_model(best_goals)
 
-best_points = compare_models(n_select = 3)
+roc_auc(goals_created, get_config('X_train'), get_config('y_train'), X_test=get_config('X_test'), y_test=get_config('y_test'), show=False)
+plt.savefig('./data/img/goals_auc.png')
+plt.clf()
 
-best_points_blended = blend_models(estimator_list = best_points, choose_better=True)
-
-best_points_stacked = stack_models(estimator_list = best_points_blended, choose_better=True)
-
-predict_model(best_points_stacked)
-
-final_best_points_model = finalize_model(best_points_stacked)
-
-save_model(final_best_points_model,'./models/final_best_points_model')
-
-points_predictions = predict_model(final_best_points_model, data=ekstraklasa_test)
+confusion_matrix(goals_created, get_config('X_train'), get_config('y_train'), X_test=get_config('X_test'), y_test=get_config('y_test'), show=False)
+plt.savefig('./data/img/goals_conf.png')
+plt.clf()
 
 
-# Applying points predictions to train goals
+# MODEL TUNING
+print("Model tuning...")
 
-X_G_bal['points'] = points_predictions['Label']
-X_Gl_bal['points'] = points_predictions['Label']
+goals_tuned = tune_model(goals_created, optimize='AUC', choose_better=True, n_iter = 50)
 
+goals_final = finalize_model(goals_tuned)
 
-# Home goals training
-
-s_home_goals = setup(data = X_G_bal, target = 'goals',
-                     ignore_features=['goals_lost'],
-                     silent=True,
-                     categorical_features = cat_feats,
-                     remove_multicollinearity = True)
+goals_all = predict_model(goals_final)
 
 
-best_home_goals = compare_models(n_select = 3)
+# SAVING MODEL
+print("Saving model...")
+save_model(goals_final, './models/goals_final')
 
-best_home_goals_blended = blend_models(estimator_list = best_home_goals, choose_better=True)
-
-best_home_goals_stacked = stack_models(estimator_list = best_home_goals_blended, choose_better=True)
-
-predict_model(best_home_goals_stacked)
-
-final_best_home_goals_model = finalize_model(best_home_goals_stacked)
-
-save_model(final_best_home_goals_model,'./models/final_best_home_goals_model')
-
-
-# Away goals training
-
-s_away_goals = setup(data = X_Gl_bal, target = 'goals_lost',
-                     ignore_features=['goals'],
-                     silent=True,
-                     categorical_features = cat_feats,
-                     remove_multicollinearity = True)
-
-
-best_away_goals = compare_models(n_select = 3)
-
-best_away_goals_blended = blend_models(estimator_list = best_away_goals, choose_better=True)
-
-best_away_goals_stacked = stack_models(estimator_list = best_away_goals_blended, choose_better=True)
-
-predict_model(best_away_goals_stacked)
-
-final_best_away_goals_model = finalize_model(best_away_goals_stacked)
-
-save_model(final_best_away_goals_model,'./models/final_best_away_goals_model')
+points_predictions.to_csv('./data/predictions/points_predictions.csv')
+goals_predictions.to_csv('./data/predictions/goals_predictions.csv')
