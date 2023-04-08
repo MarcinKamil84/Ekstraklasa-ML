bc550f4b52772d37996df3a9c9f73b840047f001
Marcin Świostek <99032084+MarcinKamil84@users.noreply.github.com>
2023-04-08 23:15:33 +0200

    Update predict_models.py

--- scripts/predict_models.py
+++ scripts/predict_models.py
@@ -4,7 +4,7 @@ import pickle
 # Loading models and variables
 
 final_best_points_model = load_model('./models/points_final')
-final_best_home_model = load_model('./models/goals_final')
+final_best_goals_model = load_model('./models/goals_final')
 
 with open("./data/variables/teams_dict.pkl", "rb") as file:
     teams_dict = pickle.load(file)
@@ -12,23 +12,23 @@ with open("./data/variables/teams_dict.pkl", "rb") as file:
 ekstraklasa_test = pd.read_pickle('./data/inputs/ekstraklasa_test.pkl')
 
 points_predictions = predict_model(final_best_points_model, data=ekstraklasa_test)
-home_predictions = predict_model(final_best_home_model, data=ekstraklasa_test)
+goals_predictions = predict_model(final_best_goals_model, data=ekstraklasa_test)
 
 
 # COMBINING THE RESULTS
 
 results_df = pd.DataFrame()
 
-results_df['dom'] = home_predictions['team']
-results_df['wyjazd'] = home_predictions['opponent']
+results_df['dom'] = goals_predictions['team']
+results_df['wyjazd'] = goals_predictions['opponent']
 
 results_df['punkty'] = points_predictions['Label']
 results_df['punkty_score'] = round(points_predictions['Score'], 2)
 
-results_df['bramki_dom'] = home_predictions['Label']
-results_df['bramki_dom_score'] = round(home_predictions['Score'], 2)
+results_df['bramki_dom'] = goals_predictions['Label']
+results_df['bramki_dom_score'] = round(goals_predictions['Score'], 2)
 
-results_df['home_or_away'] = home_predictions['home_or_away']
+results_df['home_or_away'] = goals_predictions['home_or_away']
 
 results_df_combo = results_df.merge(results_df, how='left',
                                     left_on=['dom', 'wyjazd'],
